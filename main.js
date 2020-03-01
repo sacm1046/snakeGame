ae1debf1ae2e536eb8e0f3d654c7e0be6b8dfd6f
Sebastian Contreras <sacm1046@gmail.com>
2020-03-01 19:31:00 -0300

    19:30

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-let matrizPlayer = [["p1-ta1", "p1-tb1", "p1-tc1", "p1-td1", "p1-te1", "p1-tf1", "p1-tg1", "p1-th1", "p1-ti1", "p1-tj1"],
-["p1-ta2", "p1-tb2", "p1-tc2", "p1-td2", "p1-te2", "p1-tf2", "p1-tg2", "p1-th2", "p1-ti2", "p1-tj2"],
-["p1-ta3", "p1-tb3", "p1-tc3", "p1-td3", "p1-te3", "p1-tf3", "p1-tg3", "p1-th3", "p1-ti3", "p1-tj3"],
-["p1-ta4", "p1-tb4", "p1-tc4", "p1-td4", "p1-te4", "p1-tf4", "p1-tg4", "p1-th4", "p1-ti4", "p1-tj4"],
-["p1-ta5", "p1-tb5", "p1-tc5", "p1-td5", "p1-te5", "p1-tf5", "p1-tg5", "p1-th5", "p1-ti5", "p1-tj5"],
-["p1-ta6", "p1-tb6", "p1-tc6", "p1-td6", "p1-te6", "p1-tf6", "p1-tg6", "p1-th6", "p1-ti6", "p1-tj6"],
-["p1-ta7", "p1-tb7", "p1-tc7", "p1-td7", "p1-te7", "p1-tf7", "p1-tg7", "p1-th7", "p1-ti7", "p1-tj7"],
-["p1-ta8", "p1-tb8", "p1-tc8", "p1-td8", "p1-te8", "p1-tf8", "p1-tg8", "p1-th8", "p1-ti8", "p1-tj8"],
-["p1-ta9", "p1-tb9", "p1-tc9", "p1-td9", "p1-te9", "p1-tf9", "p1-tg9", "p1-th9", "p1-ti9", "p1-tj9"],
-["p1-ta10", "p1-tb10", "p1-tc10", "p1-td10", "p1-te10", "p1-tf10", "p1-tg10", "p1-th10", "p1-ti10", "p1-tj10"]]
+let matrizPlayer = [
+    ["p1-ta1", "p1-tb1", "p1-tc1", "p1-td1", "p1-te1", "p1-tf1", "p1-tg1", "p1-th1", "p1-ti1", "p1-tj1"],
+    ["p1-ta2", "p1-tb2", "p1-tc2", "p1-td2", "p1-te2", "p1-tf2", "p1-tg2", "p1-th2", "p1-ti2", "p1-tj2"],
+    ["p1-ta3", "p1-tb3", "p1-tc3", "p1-td3", "p1-te3", "p1-tf3", "p1-tg3", "p1-th3", "p1-ti3", "p1-tj3"],
+    ["p1-ta4", "p1-tb4", "p1-tc4", "p1-td4", "p1-te4", "p1-tf4", "p1-tg4", "p1-th4", "p1-ti4", "p1-tj4"],
+    ["p1-ta5", "p1-tb5", "p1-tc5", "p1-td5", "p1-te5", "p1-tf5", "p1-tg5", "p1-th5", "p1-ti5", "p1-tj5"],
+    ["p1-ta6", "p1-tb6", "p1-tc6", "p1-td6", "p1-te6", "p1-tf6", "p1-tg6", "p1-th6", "p1-ti6", "p1-tj6"],
+    ["p1-ta7", "p1-tb7", "p1-tc7", "p1-td7", "p1-te7", "p1-tf7", "p1-tg7", "p1-th7", "p1-ti7", "p1-tj7"],
+    ["p1-ta8", "p1-tb8", "p1-tc8", "p1-td8", "p1-te8", "p1-tf8", "p1-tg8", "p1-th8", "p1-ti8", "p1-tj8"],
+    ["p1-ta9", "p1-tb9", "p1-tc9", "p1-td9", "p1-te9", "p1-tf9", "p1-tg9", "p1-th9", "p1-ti9", "p1-tj9"],
+    ["p1-ta10", "p1-tb10", "p1-tc10", "p1-td10", "p1-te10", "p1-tf10", "p1-tg10", "p1-th10", "p1-ti10", "p1-tj10"]
+]
 
 let snake = []
 let food = []
@@ -19,7 +21,12 @@ let left = 0
 let rigth = 0
 let time = 500
 let count = 0
-let save = []
+
+function finish() {
+    location.reload()
+    alert("Puntaje " + count + "\nInténtalo de nuevo")
+    count = 0
+}
 
 function posSnake() {
     randomX1 = Math.floor(Math.random() * 10)
@@ -27,7 +34,7 @@ function posSnake() {
 
     snake = [(matrizPlayer[randomX1][randomY1])]
     document.getElementById(snake[0]).style.background = "red"
-    document.getElementById('score').innerHTML=count
+    document.getElementById('score').innerHTML = count
 }
 posSnake();
 
@@ -51,25 +58,9 @@ function eaten() {
         posSnakeFood()
         time = time * 0.95
         count++
-        document.getElementById('score').innerHTML=count
-        /* if(up !== 0){
-            snake.push(matrizPlayer[randomX1+1][randomY1])
-
-        }
-        else if(down !== 0){
-            snake.push(matrizPlayer[randomX1-1][randomY1])
-
-        }
-        else if(left !== 0){
-            snake.push(matrizPlayer[randomX1][randomY1+1])
- 
-        }
-        else if(rigth !== 0){
-            snake.push(matrizPlayer[randomX1][randomY1-1])
-        }  */
+        document.getElementById('score').innerHTML = count
     }
 }
-
 function moveUp() {
     if (randomX1 > 0) {
         document.getElementById(snake[0]).style.background = "white"
@@ -81,13 +72,11 @@ function moveUp() {
         eaten()
     }
     else {
-        location.reload();
-        count = 0
-        alert("Intentalo de nuevo")
+        finish()
     }
 }
 function moveDown() {
-    if (randomX1 < 10) {
+    if (randomX1 < 9) {
         document.getElementById(snake[0]).style.background = "white"
         randomX1 = randomX1 + 1
         snake = [(matrizPlayer[randomX1][randomY1])]
@@ -97,9 +86,7 @@ function moveDown() {
         eaten()
     }
     else {
-        location.reload();
-        count = 0
-        alert("Intentalo de nuevo")
+        finish()
     }
 }
 function moveLeft() {
@@ -113,13 +100,11 @@ function moveLeft() {
         eaten()
     }
     else {
-        location.reload();
-        count = 0
-        alert("Intentalo de nuevo")
+        finish()
     }
 }
 function moveRigth() {
-    if (randomY1 < 10) {
+    if (randomY1 < 9) {
         document.getElementById(snake[0]).style.background = "white"
         randomY1 = randomY1 + 1
         snake = [(matrizPlayer[randomX1][randomY1])]
@@ -129,20 +114,15 @@ function moveRigth() {
         eaten()
     }
     else {
-        location.reload();
-        count = 0
-        alert("Intentalo de nuevo")
+        finish()
     }
 }
-
 let body = document.querySelector("body")
 body.addEventListener("keydown", (e) => {
     e.preventDefault()
     if (e.keyCode === 38) {
         if (down !== 0) {
-            location.reload();
-            count = 0
-            alert("Intentalo de nuevo")
+            finish()
         }
         else {
             clearInterval(down)
@@ -156,9 +136,7 @@ body.addEventListener("keydown", (e) => {
     }
     else if (e.keyCode === 40) {
         if (up !== 0) {
-            location.reload();
-            count = 0
-            alert("Intentalo de nuevo")
+            finish()
         }
         else {
             clearInterval(up)
@@ -172,9 +150,7 @@ body.addEventListener("keydown", (e) => {
     }
     else if (e.keyCode === 37) {
         if (rigth !== 0) {
-            location.reload();
-            count = 0
-            alert("Intentalo de nuevo")
+            finish()
         }
         else {
             clearInterval(up)
@@ -188,9 +164,7 @@ body.addEventListener("keydown", (e) => {
     }
     else if (e.keyCode === 39) {
         if (left !== 0) {
-            location.reload();
-            count = 0
-            alert("Intentalo de nuevo")
+            finish()
         }
         else {
             clearInterval(up)
